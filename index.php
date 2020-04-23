13ad128931519dbf326bd70c73ef6698f11dcfac
Ron Richardson <ron.richardson@gamil.com>
2020-04-23 01:08:03 -0400

    Quick homepage and htaccess

--- index.php
+++ index.php
@@ -1,43 +1,56 @@
-<?php
-	echo "Hello.";
-	
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Transitional Analysis 3 Age Estimation</title>
+	<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+	<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+	<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
+</head>
+<body>
 
-// 	require "lib/WebApi.php";
+	<div class="container grid-lg">
+		<div class="columns">
+			<div class="column">
+				<div class="hero hero-sm bg-dark">
+					<div class="hero-body">
+						<h1>Transitional Analysis 3</h1>
+						<p>Age Estimation with transition analysis and machine learning.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column col-8">
+				<p>
+					TA<sup>3</sup> uses transition analysis and machine learning to provide age estimation from skeletal indicators with explicit probabilities. Funded under National Institute of Justice grant 2014-DN-BK-K007.
+				</p>
+			</div>
+			<div class="column col-4">
+				<div class="card">
+					<div class="card-image">
+						<img src="app-assets/icon.png" class="img-responsive" />
+					</div>
+					<div class="card-header">
+						<div class="card-title h5">Latest Release</div>
+						<div class="card-subtitle text-gray">
+							This release has ...
+						</div>
+					</div>
+					<div class="card-footer">
+						<button class="btn btn-primary">Download</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-// 	include_once "env.php";
-// 	include_once "lib/SimpleORM.class.php";
-// 	include_once "db/db.php";
-// 	include_once "db/models.php";
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-73604903-11"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-73604903-11');
+	</script>
 
-// 	use Vanen\Mvc\Api;
-// 	use Vanen\Mvc\ApiController;
-
-// 	class AnalysisController extends ApiController
-// 	{
-// 		/** :GET :{controller}/{$id} */
-// 		public function get($id)
-// 		{
-// 			return json_encode(AnalysisLog::retrieveByid($id));
-// 		}
-
-// 		/** :POST */
-// 		public function log($data)
-// 		{
-// 			return $data;
-// 		}
-// 	}
-
-// 	class DebugController extends ApiController
-// 	{
-// 		/** :GET */
-// 		public function list()
-// 		{
-// 			return "asdflkasdfasfd";
-// 		}
-// 	}
-
-// 	$api = new Api();
-// 	$api->handle();
-?>
+</body>
+</html>
